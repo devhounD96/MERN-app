1997a4f3ded88bd5c945974bfc6aeac99cf05de3
vishnuSekhar <vishnusekhar96@gmail.com>
2022-11-14 15:38:09 +0530

    user crud

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -80,8 +80,8 @@ const  deleteUser = asyncHandler(async (req, res) => {
     if(!id) {
         return res.status(400).json({message: 'User id is required'});
     }
-    const notes = await Note.findOne({ user: id}).lean().exec();
-    if(notes?.length) {
+    const note = await Note.findOne({ user: id}).lean().exec();
+    if(note) {
         return res.status(400).json({message: 'User has assigned notes'});
     }
 
