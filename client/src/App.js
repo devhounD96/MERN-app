624bcc6458a70a7ed1de8c1701392a6c5150f57b
vishnuSekhar <vishnusekhar96@gmail.com>
2022-11-14 18:42:52 +0530

    layout- in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
+import { Routes, Route} from 'react-router-dom';
+import Layout from './components/Layout';
+import Login from './components/Login';
+import Public from './components/Public';
 function App() {
   return (
-    <div>
-     Hi
-    </div>
+   <Routes>
+    <Route path='/' element={<Layout />}>
+    <Route index element={<Public />} />
+    <Route path='login' element={<Login />} />
+    </Route>
+   </Routes>
   );
 }
 
