ec152bfc75f7fb8a5ffd7d06cefa06feb21cc570
vishnuSekhar <vishnusekhar96@gmail.com>
2022-11-19 18:11:26 +0530

    implement basic route layer skeleton

--- client/src/components/DashLayout.js
+++ client/src/components/DashLayout.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
+import DashFooter from "./DashFooter"
+import DashHeader from './DashHeader'
 const DashLayout = () => {
   return (
     <>
+    <DashHeader />
     <div className="dash-container">
         <Outlet />
     </div>
+    <DashFooter />
     </>
   )
 }
