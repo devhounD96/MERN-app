a04e135f4fc06e1b17b8f9e7b0423df6e7f5e3f0
vishnuSekhar <vishnusekhar96@gmail.com>
2022-11-20 15:20:19 +0530

    Initialize RTK

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Provider store={store}>
     <Routes>
       <Route path='/*' element={<App />} />
     </Routes>
+  </Provider>
   </BrowserRouter>
 );
